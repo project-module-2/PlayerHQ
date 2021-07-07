ad9891330d13b250eb1b5d05f183da558b47c9b8
andres_rog <andres_rog@outlook.com>
2021-07-06 20:43:19 -0500

    avances back

---
+++ playerBack/models/request.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
+
+const requestSchema = new Schema({
+    type:{
+        type:String,
+        required:[true,"Debes agregar un tipo de request"],
+        enum:["friend","party"]
+    },
+    date:{
+        type:String,
+        required:[true,"Debes agregar una fecha"]
+    },
+    message: {
+        type:String,
+        required:[true,"Debes agregar un mensaje"],
+        max:50
+    },
+    _from: {
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        require:[true,"Debes agregar el usuario que solicita la invitacion"]
+    },
+    _to:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        require:[true,"Debes agregar el usuario destinatario de la invitacion"]
+    }
+},{timestapms:true})
+
+module.exports = model("Request",requestSchema);
